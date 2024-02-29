edaee42c111428f2286db3f0b715cf86eb08d75a
Aleph <arcanforce00@gmail.com>
2024-02-29 14:30:18 -0300

    Firts commit

--- observator.js
+++ observator.js
@@ -1,31 +1,14 @@
-const observator = new IntersectionObserver(
-    entries => { // entries siempre sera un array
-        entries.forEach(entry => {
-        if(entry.isIntersecting) { // isIntersecting = "esta interseptado" / aparece en la pantalla
-            entry.target.classList.remove('hide')
-        } else {
-            entry.target.classList.add('hide')
-        }
-    }), {threshold: 0.5} // Medida del elemento en pantalla para que este aparezca (0.0 a 1.0 )
-})
-
-const galeriaLatam = document.querySelectorAll("#galeria-rol-latam .image-container .image img")
-
-galeriaLatam.forEach(e => {
-    observator.observe(e)
-})
-
 const changeImageAndTextHernan = (element, newText, newImageSrc) => {
     element.src = newImageSrc;
-    document.getElementById('textToChangeHerni').innerHTML = newText;
+    document.getElementById('textToChangeHernan').innerHTML = newText;
 }
 
 const changeImageAndTextCesar = (element, newText, newImageSrc) => {
     element.src = newImageSrc;
-    document.getElementById('textToChangeCesarito').innerHTML = newText;
+    document.getElementById('textToChangeCesar').innerHTML = newText;
 }
 
 const changeImageAndTextAleph = (element, newText, newImageSrc) => {
     element.src = newImageSrc;
-    document.getElementById('textToChangeHernanDandreis').innerHTML = newText;
+    document.getElementById('textToChangeAleph').innerHTML = newText;
 }
